e0be7726c075352fd3ccbf5046f0c7af44b0c578
Dirk Strauss <dirk.strauss@thisisembrace.com>
2019-06-03 23:11:42 +0200

    Code commit

---
+++ IntelliCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace IntelliCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserSettings.FirstName = "John";
+            UserSettings.LastName = "Doe";
+            UserSettings.DOB = DateTime.Now.AddYears(-25);
+
+            WriteLine(UserSettings.FullName());
+            WriteLine(UserSettings.GetAge());
+            WriteLine(UserSettings.GetUserGuid(UserSettings.FullName()));
+            _ = ReadLine();            
+
+        }
+    }
+}
---
+++ IntelliCode/UserSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IntelliCodeDemo
+{
+    public static class UserSettings
+    {
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+        public static DateTime DOB { get; set; }
+
+        public static Guid GetUserGuid(string value)
+        {            
+            Guid GenerateGuid()
+            {
+                using (var md5 = MD5.Create())
+                {
+                    var hash = md5.ComputeHash(Encoding.Default.GetBytes(value));
+                    return new Guid(hash);
+                }                
+            }
+
+            return GenerateGuid();
+        }
+
+        public static string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public static int GetAge()
+        {            
+            var age = DateTime.Now - DOB;
+            return (int)(age.Days / 365.25);
+        }
+
+    }
+}
